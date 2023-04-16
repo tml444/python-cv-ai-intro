8712bda795b8a8e59a6e3521bf6b88d6b74e1cd7
Timo Leitritz <timo.leitritz@ipa.fraunhofer.de>
2023-04-16 17:22:59 +0200

    Update readme install

---
+++ 3_ai_basics_with_jupyter/c_webcam_object_detection.py
@@ -0,0 +1,51 @@
+# import the opencv library, which is called "cv2"
+import cv2
+import mediapipe
+
+# initialize hand detector from mediapipe
+hand_detector = mediapipe.solutions.hands.Hands()
+
+# create a webcam object from webcam with id "0"
+webcam = cv2.VideoCapture(1)
+
+# print some info how to control this program
+print("Press q to end this program.")
+
+# setup a image_counter to count the amount of images displayed
+image_counter = 0
+
+# start a while loop that runs as long as it's condition is "TRUE". In this case it will run infinitely if not stopped with "break"
+while True:
+    
+    # try to get an image from the webcam, if this try was successful, it will be saved in the variable "success"  and the image is saved in "image"
+    success, image = webcam.read()
+    
+    # print info about success if image read
+    print("Image successfully received? ->", success)
+    
+    # stop the loop ("break") if the image was not successfully received, so the variable success is "False"
+    if success == False:
+        break
+    
+    # print current counter value
+    print("Show image nr.", image_counter)
+    
+    # detect hand using ai (with mediapipe library)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    result = hand_detector.process(image_rgb)
+    print(result)
+    
+    # display the received image in a window
+    cv2.imshow("My Webcam Window", image)
+    cv2.imshow("Hand detector", image_rgb)
+    
+    # wait indefinitely for a key press on the keyboard, if key is pressed, save the key that was pressed in "pressed_key" variable and continue
+    pressed_key = cv2.waitKey(0)
+    
+    # if the presed key was the q key, stop the loop
+    if pressed_key == ord("q"):
+        break
+    
+    # add 1 to the image counter to count the image
+    image_counter = image_counter + 1
+
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # python-cv-ai-intro
 
 Small introduction to python, opencv and ai. See `docs/Anleitung.pdf` directory for a guide how to use this repository.
+
+## Setup
+
+1. Open a command prompt or terminal somewhere
+2. Create conda environment by running the following command:
+
+`conda create -n cvintro python=3.7 -y`
+
+3. Activate the environment with:
+
+`conda activate cvintro`
+
+4. Install dependencies:
+
+`pip install tensorflow opencv-python mediapipe matplotlib`
+
+5. Open Visual Studio Code and open the directory where you cloned this repository. Start with 1_python_basics and go on with 2.. and 3..
