9ffcd3e8637534fefea8ace89aa01cc31f8eda06
Timo Leitritz <timo.leitritz@ipa.fraunhofer.de>
2023-04-20 15:57:01 +0200

    Add playground python basics

---
+++ 1_python_basics/c_playground.py
@@ -0,0 +1,9 @@
+# Try out your own program! For example, a loop that counts from 1 to 1000, like this...
+# Remove the "#" in front of the lines to "activate" the code. This is called "uncomment" the code and will be useful later.
+
+
+# counter = 0
+# while counter <= 1000:
+#     print(counter)
+#     counter+=1
+    
