3b403f14683e7eea8b931a0845c292ce761294c3
Timo Leitritz <timo.leitritz@ipa.fraunhofer.de>
2023-04-20 16:27:07 +0200

    Add drawing to object hand detection

--- 3_ai_basics_with_jupyter/c_webcam_object_detection_drawing.py
+++ 3_ai_basics_with_jupyter/c_webcam_object_detection_drawing.py
@@ -2,6 +2,8 @@
 import cv2
 import mediapipe
 
+import numpy as np
+
 # initialize hand detector from mediapipe
 hand_detector = mediapipe.solutions.hands.Hands()
 
@@ -15,11 +17,17 @@ print("Press q to end this program.")
 # setup a image_counter to count the amount of images displayed
 image_counter = 0
 
+# create canvas image variable
+canvas_image = None
+
 # start a while loop that runs as long as it's condition is "TRUE". In this case it will run infinitely if not stopped with "break"
 while True:
     
     # try to get an image from the webcam, if this try was successful, it will be saved in the variable "success"  and the image is saved in "image"
     success, image = webcam.read()
+    
+    if canvas_image is None:
+        canvas_image = np.zeros_like(image)
     
     # print info about success if image read
     print("Image successfully received? ->", success)
@@ -39,10 +47,17 @@ while True:
     if results.multi_hand_landmarks is not None:
         for hand_landmarks in results.multi_hand_landmarks:
             mediapipe.solutions.drawing_utils.draw_landmarks(image_rgb, hand_landmarks)
+            
+            finger_zero_x = int(hand_landmarks.landmark[8].x * canvas_image.shape[1])
+            finger_zero_y = int(hand_landmarks.landmark[8].y * canvas_image.shape[0])
+            
+            canvas_image = cv2.drawMarker(canvas_image, (finger_zero_x, finger_zero_y), (0,255,0), cv2.MARKER_DIAMOND, 3, 2)
     
     # display the received image in a window
-    cv2.imshow("My Webcam Window", image)
-    cv2.imshow("Hand detector", image_rgb)
+    cv2.imshow("Draw something!", canvas_image)
+    
+    image_bgr = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR)
+    cv2.imshow("Hand detector", image_bgr)
     
     # wait indefinitely for a key press on the keyboard, if key is pressed, save the key that was pressed in "pressed_key" variable and continue
     pressed_key = cv2.waitKey(1)
@@ -50,6 +65,10 @@ while True:
     # if the presed key was the q key, stop the loop
     if pressed_key == ord("q"):
         break
+    
+    # if the presed key was the x key, reset canvas image
+    if pressed_key == ord("x"):
+        canvas_image = None
     
     # add 1 to the image counter to count the image
     image_counter = image_counter + 1
